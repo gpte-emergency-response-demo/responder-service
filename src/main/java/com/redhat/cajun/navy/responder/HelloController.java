3074f4cd2d4e719eceefdf48c3cedd24703244d3
Dillon Winchell <dwinchel@redhat.com>
2019-01-09 11:29:59 -0500

    Added Spring boot skeleton from Mike Evarria - https://github.com/mechevarria/ocp-sso/blob/master/springboot/pom.xml.

---
+++ src/main/java/com/redhat/cajun/navy/responder/HelloController.java
@@ -0,0 +1,23 @@
+package com.redhat.cajun.navy.responder;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/springboot-api")
+	public HashMap<String, String> index() {
+
+		HashMap<String, String> model = new HashMap<>();
+
+		model.put("status", "Greetings from Spring Boot!");
+		model.put("time", LocalDateTime.now().toString());
+
+		return model;
+	}
+
+}
