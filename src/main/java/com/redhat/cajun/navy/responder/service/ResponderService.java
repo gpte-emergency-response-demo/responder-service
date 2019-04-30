e9c31d5970e2756999b71f94dae308731ff220d1
Bernard Tison <bernard.tison@gmail.com>
2019-04-30 08:39:40 +0200

    Added enrolled field to conversion between model class and entity

--- src/main/java/com/redhat/cajun/navy/responder/service/ResponderService.java
+++ src/main/java/com/redhat/cajun/navy/responder/service/ResponderService.java
@@ -143,6 +143,7 @@ public class ResponderService {
                 .boatCapacity(responder.getBoatCapacity())
                 .medicalKit(responder.isMedicalKit())
                 .available(responder.isAvailable())
+                .enrolled(responder.isEnrolled())
                 .person(responder.isPerson())
                 .build();
     }
@@ -162,6 +163,7 @@ public class ResponderService {
                 .medicalKit(responder.isMedicalKit() == null ? current.getMedicalKit() : responder.isMedicalKit())
                 .available(responder.isAvailable() == null ? current.isAvailable() : responder.isAvailable())
                 .person(responder.isPerson() == null ? current.isPerson() : responder.isPerson())
+                .enrolled(responder.isEnrolled() == null ? current.isEnrolled() : responder.isEnrolled())
                 .build();
     }
 
@@ -180,6 +182,7 @@ public class ResponderService {
                 .medicalKit(responder.getMedicalKit())
                 .available(responder.isAvailable())
                 .person(responder.isPerson())
+                .enrolled(responder.isEnrolled())
                 .build();
     }
 }
